d40072a2c377403991769012af09eb767f63e5c6
Matías Cellamare <88092713+cellamarematias@users.noreply.github.com>
2022-08-15 13:56:12 -0300

    Server: set headers - acces control allow origin

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(Router);
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+    });
 
 app.get('/', (req, res) => {
     res.send('Mati server');

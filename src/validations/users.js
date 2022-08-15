2a315fbd50b910534b63e05ac722d3aca43b3567
Matías Cellamare <88092713+cellamarematias@users.noreply.github.com>
2022-08-14 21:26:59 -0300

    Server: add vercel.json

--- src/validations/users.js
+++ src/validations/users.js
@@ -1,23 +1,23 @@
-// import Joi from 'Joi';
+import Joi from 'Joi';
 
-// const usersValidation = (req, res, next) => {
-//   const conditions = Joi.object({
-//     _id: Joi.string().required(),
-//     fullName: Joi.string().min(3).max(200).required(),
-//     email: Joi.string().min(3).max(200).required(),
-//     couples: Joi.array(),
-//   });
-//   const validationUsers = conditions.validate(req.body);
-//   if (validationUsers.error) {
-//     return res.status(400).json({
-//       message: validationUsers.error.message,
-//       error: true,
-//       data: undefined,
-//     });
-//   }
-//   return next();
-// };
+const usersValidation = (req, res, next) => {
+  const conditions = Joi.object({
+    _id: Joi.string().required(),
+    fullName: Joi.string().min(3).max(200).required(),
+    email: Joi.string().min(3).max(200).required(),
+    couples: Joi.array(),
+  });
+  const validationUsers = conditions.validate(req.body);
+  if (validationUsers.error) {
+    return res.status(400).json({
+      message: validationUsers.error.message,
+      error: true,
+      data: undefined,
+    });
+  }
+  return next();
+};
 
-// export default {
-//     usersValidation,
-// };
+export default {
+    usersValidation,
+};
